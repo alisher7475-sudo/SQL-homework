88ab848a88aa70582ddd08fb688e9a40061dc646
alisher7475-sudo <alisher7475@gmail.com>
2025-09-09 16:06:10 +0500

    Update lesson1.sql

--- lesson-1/homework/lesson1.sql
+++ lesson-1/homework/lesson1.sql
@@ -1,6 +1,9 @@
 Easy Task1
 Question1  Data- сақланиши ва қайта ишланиши мумкин бўлган фактлар, рақамлар ёки ҳар қандай маълумот.(facts, figures, or any information that can be stored and processed.)
  Database-маълумотларни сақлаш ва бошқариш учун ташкил этилган жой.(an organized collection of data where information is stored and managed.)
-Task3. Relational database-(Relational database) – маълумотлар жадваллар (relation) кўринишида сақланадиган база.(a database in which data is stored in related tables (relations).)
-Task4. Table-маълумотлар базасидаги структура бўлиб, у қаторлар (ёзувлар) ва устунлардан (майдонлардан) иборат (a structure in a database consisting of rows (records) and columns (fields) where data is stored.)
-Task2
+Task3. Relational database-(Relational database) – маълумотлар жадваллар (relation) кўринишида сақланадиган база.(a database in which data is stored in related tables (relations).) Table-маълумотлар базасидаги структура бўлиб, у қаторлар (ёзувлар) ва устунлардан (майдонлардан) иборат (a structure in a database consisting of rows (records) and columns (fields) where data is stored.)
+Task2 Юқори самарадорлик ва масштабланиш имконияти.
+Транзакцияларни қўллаб-қувватлаш ва маълумотлар яхлитлигини таъминлаш.
+Захира нусха олиш ва тиклаш имкониятлари.
+Сақланувчи процедуралар, триггерлар ва функцияларни қўллаб-қувватлайди.
+Бизнес таҳлил (BI) ва ҳисобот воситалари билан интеграция.
