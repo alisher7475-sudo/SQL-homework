95f36c6b2f25c31efde0540cdd48c1a0b7090980
alisher7475-sudo <alisher7475@gmail.com>
2025-09-09 16:30:59 +0500

    Update lesson1.sql

--- lesson-1/homework/lesson1.sql
+++ lesson-1/homework/lesson1.sql
@@ -1,9 +1,19 @@
 Easy Task1
 Question1  Data- сақланиши ва қайта ишланиши мумкин бўлган фактлар, рақамлар ёки ҳар қандай маълумот.(facts, figures, or any information that can be stored and processed.)
  Database-маълумотларни сақлаш ва бошқариш учун ташкил этилган жой.(an organized collection of data where information is stored and managed.)
-Task3. Relational database-(Relational database) – маълумотлар жадваллар (relation) кўринишида сақланадиган база.(a database in which data is stored in related tables (relations).) Table-маълумотлар базасидаги структура бўлиб, у қаторлар (ёзувлар) ва устунлардан (майдонлардан) иборат (a structure in a database consisting of rows (records) and columns (fields) where data is stored.)
-Task2 Юқори самарадорлик ва масштабланиш имконияти.
+ Relational database-(Relational database) – маълумотлар жадваллар (relation) кўринишида сақланадиган база.(a database in which data is stored in related tables (relations).) Table-маълумотлар базасидаги структура бўлиб, у қаторлар (ёзувлар) ва устунлардан (майдонлардан) иборат (a structure in a database consisting of rows (records) and columns (fields) where data is stored.)
+  Task2 Юқори самарадорлик ва масштабланиш имконияти.
 Транзакцияларни қўллаб-қувватлаш ва маълумотлар яхлитлигини таъминлаш.
 Захира нусха олиш ва тиклаш имкониятлари.
 Сақланувчи процедуралар, триггерлар ва функцияларни қўллаб-қувватлайди.
 Бизнес таҳлил (BI) ва ҳисобот воситалари билан интеграция.
+TaTask3 Windows аутентификацияси – Windows’даги ҳисоб орқали уланиш.
+SQL Server аутентификацияси – SQL Server’да яратилган логин ва пароль орқали уланиш.
+ MMedium Task1 
+CrCreate a new database in SSMS named SchoolDB.
+ StudentID (INT, PRIMARY)
+Name (VARCHAR(50))
+Age (INT)
+SQL Server – это система управления базами данных (СУБД), разработанная Microsoft. Она хранит, обрабатывает и управляет данными.
+SSMS (SQL Server Management Studio) – это инструмент/программа с графическим интерфейсом для работы с SQL Server: написание запросов, администрирование, настройка.
+SQL (Structured Query Language) – это язык запросов для работы с данными: создание таблиц, выборка, обновление, удаление. SQL — это сам язык, а SQL Server — программа, которая его понимает.
