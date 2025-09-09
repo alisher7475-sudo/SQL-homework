e1923264859dbe950eb878ea7f2b39907dc733fd
alisher7475-sudo <alisher7475@gmail.com>
2025-09-09 09:39:01 +0500

    Update lesson1.sql

--- lesson-1/homework/lesson1.sql
+++ lesson-1/homework/lesson1.sql
@@ -1,1 +1,6 @@
- aziza Narcis
+Easy Task1
+Question1  Data- сақланиши ва қайта ишланиши мумкин бўлган фактлар, рақамлар ёки ҳар қандай маълумот.(facts, figures, or any information that can be stored and processed.)
+ Database-маълумотларни сақлаш ва бошқариш учун ташкил этилган жой.(an organized collection of data where information is stored and managed.)
+Task3. Relational database-(Relational database) – маълумотлар жадваллар (relation) кўринишида сақланадиган база.(a database in which data is stored in related tables (relations).)
+Task4. Table-маълумотлар базасидаги структура бўлиб, у қаторлар (ёзувлар) ва устунлардан (майдонлардан) иборат (a structure in a database consisting of rows (records) and columns (fields) where data is stored.)
+Task2
